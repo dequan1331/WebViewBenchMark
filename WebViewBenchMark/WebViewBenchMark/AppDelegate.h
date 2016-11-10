d01afc25e1d58d11c85545c3b18cd4ffa0786a2a
dequan1331 <dequanzhu@dequanzhudeiMac.local>
2016-11-10 10:26:13 +0800

    initial commit

---
+++ WebViewBenchMark/WebViewBenchMark/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewBenchMark
+//
+//  Created by dequanzhu on 2016/11/10.
+//  Copyright © 2016年 dequanzhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewBenchMark/WebViewBenchMark/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebViewBenchMark
+//
+//  Created by dequanzhu on 2016/11/10.
+//  Copyright © 2016年 dequanzhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
